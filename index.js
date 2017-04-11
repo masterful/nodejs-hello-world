d62aa860ad74d719a79aa3b76ab3ffcc531e286a
Pim Vermeyden <pim2345@gmail.com>
2017-04-11 13:28:29 -0400

    allow for some logging

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const http = require('http');
 const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/plain" });
   response.end("hello world");
+
+  // Put a message in the logs as well unless that's been disabled:
+  if (process.env.NO_DEBUG) { return; }
+  console.log(`Requested ${request.url}`);
 });
 
 server.listen(80);
+console.log('Server is now listening');
